afd1782d8118dfedf629652d48fc01858a462364
Hishamct <hishamct123@gmail.com>
2026-09-21 14:49:43 +0000

    Add eduintelai.in to allowed CORS origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,7 +18,7 @@ app = FastAPI(title="EduIntel AI")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "http://13.205.55.115", "https://eduintelai.in", "https://www.eduintelai.in"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
